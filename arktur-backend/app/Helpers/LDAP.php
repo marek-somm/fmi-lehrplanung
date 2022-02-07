dded6451265c2916267d95556193ec8000cce931
darafsa <marek1999@online.de>
2022-02-07 10:36:00 +0100

    auth test

--- arktur-backend/app/Helpers/LDAP.php
+++ arktur-backend/app/Helpers/LDAP.php
@@ -43,8 +43,6 @@ class LDAP {
 					foreach ($columns as $col) {
 						if(array_key_exists($col, $entries[$i])) {
 							if($entries[$i][$col]["count"] == 1) {
-								$data[$col] = $entries[$i][$col]["0"];
-							} else {
 								$data[$col] = array();
 								for($i=0; $i<$entries[$i][$col]['count']; $i++) {
 									$data[$col][] = $entries[$i][$col];
