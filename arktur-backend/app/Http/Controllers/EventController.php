9fc0a726b69117095b98be5ff2c3f85a800f8024
darafsa <marek1999@online.de>
2024-03-18 15:19:29 +0100

    formatting

--- arktur-backend/app/Http/Controllers/EventController.php
+++ arktur-backend/app/Http/Controllers/EventController.php
@@ -8,63 +8,63 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Event;
 
 class EventController extends Controller {
-    private function in_sub_array($needle, $haystack, $identifier) {
-        foreach ($haystack as $elem) {
-            if ($needle == $elem[$identifier]) {
-                return true;
-            }
-        }
-        return false;
-    }
+	private function in_sub_array($needle, $haystack, $identifier) {
+		foreach ($haystack as $elem) {
+			if ($needle == $elem[$identifier]) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public function getEvent(Request $request) {
-        $request->validate([
-            'id' => ['required', 'integer'],
-        ]);
+	public function getEvent(Request $request) {
+		$request->validate([
+			'id' => ['required', 'integer'],
+		]);
 
-		  $selection = ['id', 'active', 'rotation', 'semester', 'sws', 'title', 'type', 'vnr', 'extra'];
+		$selection = ['id', 'active', 'rotation', 'semester', 'sws', 'title', 'type', 'vnr', 'extra'];
 
-        $event = Event::select('id', 'active', 'rotation', 'semester', 'sws', 'title', 'type', 'vnr', 'extra', 'room', 'time', 'exam')
-            ->where('id', $request->id)
-            ->get()
-            ->first();
+		$event = Event::select('id', 'active', 'rotation', 'semester', 'sws', 'title', 'type', 'vnr', 'extra', 'room', 'time', 'exam')
+			->where('id', $request->id)
+			->get()
+			->first();
 
-        if (!$event) {
-            return response("Event not found", 404);
-        }
+		if (!$event) {
+			return response("Event not found", 404);
+		}
 
-        $people = Event::find($event->id)
-            ->users()
-            ->select('displayname', 'forename', 'surname', 'email')
-            ->get();
+		$people = Event::find($event->id)
+			->users()
+			->select('displayname', 'forename', 'surname', 'email')
+			->get();
 
-        $users = Event::find($event->id)
-            ->users()
-            ->select('uid')
-            ->get()
-            ->toArray();
+		$users = Event::find($event->id)
+			->users()
+			->select('uid')
+			->get()
+			->toArray();
 
 
-        if (Auth::user()) {
-            $user = Auth::user()->uid;
+		if (Auth::user()) {
+			$user = Auth::user()->uid;
 
-            $event["own"] = false;
-            if ($this->in_sub_array($user, $users, 'uid')) {
-                $event["own"] = true;
-            }
-        }
+			$event["own"] = false;
+			if ($this->in_sub_array($user, $users, 'uid')) {
+				$event["own"] = true;
+			}
+		}
 
-        $modules = Event::find($event->id)
-            ->modules()
-            ->select('code as modulecode')
-            ->get();
+		$modules = Event::find($event->id)
+			->modules()
+			->select('code as modulecode')
+			->get();
 
-        $response = [
-            'content' => $event,
-            'people' => $people,
-            'modules' => $modules
-        ];
+		$response = [
+			'content' => $event,
+			'people' => $people,
+			'modules' => $modules
+		];
 
-        return response($response, 200);
-    }
+		return response($response, 200);
+	}
 }
